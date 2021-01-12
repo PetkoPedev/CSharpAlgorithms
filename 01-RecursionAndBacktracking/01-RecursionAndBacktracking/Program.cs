91ac5f9780757340d4f1808fb52a84823fbe8d56
Petko Pedev <petko.pedev@gmail.com>
2021-01-12 16:30:04 +0200

    Initial commit.

---
+++ 01-RecursionAndBacktracking/01-RecursionAndBacktracking/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_RecursionAndBacktracking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(CalcSum(array, 0));
+        }
+
+        private static int CalcSum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + CalcSum(array, index + 1);
+        }
+    }
+}
